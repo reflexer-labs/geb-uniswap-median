ec418d82b348fe8b009b20b38ed4d04110d4ac1b
bkellerman <bert@ciphertrace.com>
2020-09-07 13:23:31 -0700

    debugging

--- src/uni/UniswapV2Factory.sol
+++ src/uni/UniswapV2Factory.sol
@@ -27,11 +27,11 @@ contract UniswapV2Factory is IUniswapV2Factory {
         // require(token0 != address(0), 'UniswapV2: ZERO_ADDRESS');
         // require(getPair[token0][token1] == address(0), 'UniswapV2: PAIR_EXISTS'); // single check is sufficient
         pair = address(new UniswapV2Pair());
-        IUniswapV2Pair(pair).initialize(token0, token1);
-        getPair[token0][token1] = pair;
-        getPair[token1][token0] = pair; // populate mapping in the reverse direction
-        allPairs.push(pair);
-        emit PairCreated(token0, token1, pair, allPairs.length);
+        //IUniswapV2Pair(pair).initialize(token0, token1);
+        //getPair[token0][token1] = pair;
+        //getPair[token1][token0] = pair; // populate mapping in the reverse direction
+        //allPairs.push(pair);
+        //emit PairCreated(token0, token1, pair, allPairs.length);
     }
 
     function setFeeTo(address _feeTo) override external {
